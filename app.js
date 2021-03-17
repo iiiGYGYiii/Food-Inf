9d67bfc0a1ae1f4183edfb6c6656e6c712e289fd
iiiGYGYiii <realGYGY@gmail.com>
2021-03-16 19:42:28 -0600

    Created the GUI core of the WebApp

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+import express from 'express';
 
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+app.route('/')
+.get((req,res)=>{
+    res.render("home", {
+        theTitle: 'Inicio'
+    })
+});
+
+app.route("/about")
+.get((req,res)=>{
+    res.render("about", {
+        theTitle: "Sobre Mí"
+    })
+});
+
+
+app.listen(3000 | process.env.PORT, ()=>{
+    console.log("Aplicación corriendo en el puerto 3000");
+})
